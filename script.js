d95a478232959eecdb752a10078db842d988fb15
Nazwa Az-Zahra <nazwa.azzahra.tik24@stu.pnj.ac.id>
2026-05-31 16:50:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,86 +4,86 @@ const produkData = [
 
 {
 nama: "Gantungan Kunci Rajut Daur Ulang",
-deskripsi: "Gantungan kunci handmade dari benang rajut berbahan daur ulang.",
-gambar: "img/img60_.jpg"
+deskripsi: "Gantungan kunci handmade dari benang rajut berbahan daur ulang, dengan berbagai karakter lucu yang dibuat dengan detail dan kreativitas, cocok digunakan sebagai hiasan tas, kunci, maupun aksesoris sehari-hari.",
+gambar: "img/Ganci.png"
 },
 
 {
 nama: "Tas Belanja Plastik Minyak Bekas",
 deskripsi: "Tas ramah lingkungan hasil daur ulang kemasan plastik minyak goreng.",
-gambar: "img/img60.jpg"
+gambar: "img/TasMinyak.png"
 },
 
 {
-nama: "Tas Rajut Handmade",
-deskripsi: "Tas rajut handmade unik untuk penggunaan sehari-hari.",
-gambar: "img/img66.jpg"
+nama: "Tas Jinjing Rajut Handmade",
+deskripsi: "Tas rajut handmade yang dibuat dari benang pilihan dengan motif bunga yang unik, menghasilkan tampilan yang menarik dan nyaman digunakan sehari-hari.",
+gambar: "img/Jinjing.png"
 },
 
 {
 nama: "Tempat Tisu Kain Perca",
-deskripsi: "Tempat tisu cantik dari kain perca dan bahan bekas.",
-gambar: "img/img50.jpg"
+deskripsi: "Tempat tisu cantik dari kain perca dan bahan bekas, diolah menjadi produk yang unik, fungsional, dan cocok digunakan untuk kebutuhan sehari-hari.",
+gambar: "img/Tissue.png"
 },
 
 {
 nama: "Tas Daur Ulang Plastik Kemasan",
-deskripsi: "Tas multifungsi dari plastik bekas yang disetrika.",
-gambar: "img/img55.jpg"
+deskripsi: "Tas multifungsi dari plastik bekas yang disetrika kemudian dibuat menjadi tas unik,kuat dan pastinya ramah lingkungan.",
+gambar: "img/TasDaurUlangPlastik.png"
 },
 
 {
 nama: "Sarung Tangan Oven",
-deskripsi: "Sarung tangan oven dari kain bekas yang aman digunakan saat memasak.",
-gambar: "img/img59.jpg"
+deskripsi: "Sarung tangan oven terbuat dari kain bekas yang aman digunakan saat memasak.",
+gambar: "img/SarungTangan.png"
 },
 
 {
 nama: "Tas Selempang Rajut Handmade",
 deskripsi: "Tas Selempang rajut unik terbuat dari benang pakaian bekas.",
-gambar: "img/img73.jpg"
+gambar: "img/SlingBag.png"
 },
 
 {
 nama: "Bros Handmade Daur Ulang",
-deskripsi: "Bros cantik dari kombinasi kain dan bahan daur ulang.",
-gambar: "img/img40.jpg"
+deskripsi: "Bros cantik dari kombinasi kain dan bahan daur ulang, cocok digunakan sebagai hiasan atau aksesoris tambahan.",
+gambar: "img/Bros.png"
 },
 
 {
 nama: "Pouch Serut Handmade",
-deskripsi: "Pouch serut multifungsi untuk menyimpan barang kecil.",
-gambar: "img/img46.jpg"
+deskripsi: "Pouch serut multifungsi untuk menyimpan barang kecil dibuat dari benang pilihan dengan desain unik, ringan, dan praktis.",
+gambar: "img/Serut.png"
 },
 
 {
 nama: "Tempat Tumbler Rajut",
-deskripsi: "Tempat tumbler rajut handmade dengan desain unik.",
-gambar: "img/img25.jpg"
+deskripsi: "Tas botol rajut handmade yang dibuat dari benang pilihan, menghasilkan produk yang ringan, praktis, dan cocok digunakan untuk membawa botol minum sehari-hari.",
+gambar: "img/TasTumbler.png"
 },
 
 {
-nama: "Eco Enzyme",
-deskripsi: "Cairan serbaguna ramah lingkungan hasil fermentasi bahan organik.",
-gambar: "img/img34.jpg"
+nama: "Cairan Eco Enzyme",
+deskripsi: "Cairan serbaguna ramah lingkungan terbuat dari bahan alami seperti sisa buah dan sayur yang difermentasi menjadi cairan serbaguna yang efektif, aman, dan bermanfaat untuk berbagai kebutuhan.",
+gambar: "img/CairanEcoEnzym.png"
 },
 
 {
 nama: "Sabun Eco Enzyme",
-deskripsi: "Sabun ramah lingkungan berbahan eco enzyme.",
-gambar: "img/img13.jpg"
+deskripsi: "Sabun ramah lingkungan berbahan eco enzyme yang dibuat dari bahan alami pilihan, membantu membersihkan kulit dengan lembut serta memberikan sensasi segar saat digunakan.",
+gambar: "img/Sabun.png"
 },
 
 {
 nama: "Pouch Plastik Daur Ulang",
-deskripsi: "Pouch dari plastik bekas yang diolah kembali menjadi produk baru.",
-gambar: "img/img23.jpg"
+deskripsi: "Pouch dari plastik bekas yang diolah kembali menjadi produk baru, serta cocok digunakan untuk menyimpan berbagai kebutuhan sehari-hari",
+gambar: "img/Pouch.png"
 },
 
 {
 nama: "Penanda Buku Rajut",
-deskripsi: "Penanda buku atau Al-Qur'an handmade berbahan rajut.",
-gambar: "img/img4.jpg"
+deskripsi: "Penanda buku atau Al-Qur'an dibuat dari benang rajut pilihan dengan desain sederhana dan menarik, membantu memudahkan menandai halaman bacaan.",
+gambar: "img/Penanda.png"
 }
 
 ];
